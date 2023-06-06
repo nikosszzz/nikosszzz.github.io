c9e3eb1d91f759917445c4d1c61753a2a54f0b0d
nikosszzz <19nikospap@gmail.com>
2023-06-06 23:59:56 +0300

    fix: use client on navbar only

--- next.config.js
+++ next.config.js
@@ -14,7 +14,6 @@ const nextConfig = {
     experimental: {
         optimizeCss: true,
         optimisticClientCache: true
-
     }
 }
 
--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useState } from 'react';
 import { padding } from '../styles';
 import Link from "next/link";
