872d0b8a6b93397d34c0f3d5b2407897c8fc5028
nikosszzz <19nikospap@gmail.com>
2023-06-11 17:16:13 +0300

    info: add url for each account card

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -32,7 +32,8 @@ const accountData = [
         ),
         type: "twitter",
         name: "nikosszzz",
-        description: "My Twitter account."
+        description: "My Twitter account.",
+        url: "https://twitter.com/nikosszzz"
     },
     {
         icon: (
@@ -42,7 +43,8 @@ const accountData = [
         ),
         type: "github",
         name: "nikosszzz",
-        description: "My GitHub Account."
+        description: "My GitHub Account.",
+        url: "https://github.com/nikosszzz"
     }
 ]
 
@@ -88,7 +90,7 @@ export default function Info() {
                 </p>
                 {accountData.map((d) => (
                     <div key={d.type} className="mt-4 mb-4 p-5 rounded-lg shadow bg-primary">
-                        <a href="https://github.com/nikosszzz" className="flex flex-row">
+                        <a href={d.url} className="flex flex-row">
                             {d.icon}
                             <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{d.name}</h5>
                         </a>
@@ -96,7 +98,7 @@ export default function Info() {
                             <span className="mr-[5px]">
                                 {d.description}
                             </span>
-                            <a href="https://github.com/nikosszzz" className="ml-auto place-self-center h-5 inline-flex items-center text-blue-600 hover:underline bg-secondary rounded-xl pl-[6px] pr-[6px]">
+                            <a href={d.url} className="ml-auto place-self-center h-5 inline-flex items-center text-blue-600 hover:underline bg-secondary rounded-xl pl-[6px] pr-[6px]">
                                 Go
                                 <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
                             </a>
