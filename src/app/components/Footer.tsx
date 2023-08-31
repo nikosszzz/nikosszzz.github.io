6378ef19649c84f3e1bdc9ff74f8b28c85fee96a
nikosszzz <19nikospap@gmail.com>
2023-08-31 15:20:45 +0300

    info: some changes

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image"
+
 export default function Footer() {
     return (
-        <footer className="bottom-0 rounded-lg shadow m-4 bg-[#292e2e]">
-            <div className="w-full mx-auto max-w-screen-xl pt-2 pb-2">
-                <div className="text-xl mr-auto ml-auto text-center"> Made with ❤️ using Tailwind and NextJS </div>
+        //#292e2e - old color
+        <footer className="bottom-0 rounded-lg shadow m-4 bg-[#303636]">
+            <div className="w-full mx-auto max-w-screen-xl pt-2 pb-2 flex">
+                <div className="text-xl mr-auto ml-auto text-center] flex">
+                    Made with ❤️ using
+                    {<Image src={`https:/skillicons.dev/icons?i=tailwind`} height={42} width={42} alt={"Tailwind CSS"} className="pr-[5px] pl-[5px]" />}
+                    and
+                    {<Image src={`https:/skillicons.dev/icons?i=next`} height={42} width={42} alt={"NextJS"} className="pr-[5px] pl-[5px]" />}
+                </div>
             </div>
         </footer>
     )
